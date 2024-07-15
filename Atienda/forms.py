327108fabcddae96beffd97de22be9fc4134413a
Kuroh <to.garciab@duocuc.cl>
2024-07-14 22:11:32 -0400

    forms

---
+++ Atienda/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Cliente
+
+from django.forms import ModelForm
+
+class ClienteForm(ModelForm):
+    class Meta:
+        model = Cliente
+        fields = "__all__"
